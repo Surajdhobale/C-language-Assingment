87b7adf75104f266634c4dfefd251f69f689f20a
Surajdhobale <147848739+Surajdhobale@users.noreply.github.com>
2023-10-20 13:48:34 +0530

    Add files via upload

---
+++ variables.c.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    // variable declarations
+    int i = 5;
+    float f = 3.9f;
+    double d = 8.041997;
+    char c = 'A';
+    
+	//code
+    printf("\n\n");
+    
+    printf("i = %d\n", i);
+    printf("f = %f\n", f);
+    printf("d = %lf\n", d);
+    printf("c = %c\n", c);
+
+    printf("\n\n");
+
+    i = 43;
+    f = 6.54f;
+    d = 26.1294;
+    c = 'P';
+    
+    printf("i = %d\n", i);
+    printf("f = %f\n", f);
+    printf("d = %lf\n", d);
+    printf("c = %c\n", c);
+
+    printf("\n\n");
+
+    return(0);
+}
