fa0a030b6fa1721ef2ca8eec1b604c715714a440
Surajdhobale <147848739+Surajdhobale@users.noreply.github.com>
2023-10-20 13:58:16 +0530

    Add files via upload

---
+++ 06-Division/division.c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+ int a = 10;
+ int b = 20;
+ int div = 0;
+ 
+ div = a / b;
+ 
+ printf("a value =%d\n",a);
+ printf("b value =%d\n",b);
+ printf("div value =%d\n",div);
+ 
+ printf("a address =%d\n",&a);
+ printf("b address =%d\n",&b);
+ printf("div address =%d\n",&div);
+ 
+ return (0); 
+}
