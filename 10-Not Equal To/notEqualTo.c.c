fa647c04428d3fe6ac8cff1f27bd558f7e11a375
Surajdhobale <147848739+Surajdhobale@users.noreply.github.com>
2023-10-20 14:03:22 +0530

    Add files via upload

---
+++ 10-Not Equal To/notEqualTo.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    
+    
+    int a = 9, b = 10;
+    
+    
+    if(a != b)
+    {
+        printf("a is not equal to b");
+    }
+    else
+    {
+        printf("a is equal to b");
+    }
