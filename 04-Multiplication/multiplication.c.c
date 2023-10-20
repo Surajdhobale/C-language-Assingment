0d1a019e24e0d222dbc8e7739600a1d2f169eaa3
Surajdhobale <147848739+Surajdhobale@users.noreply.github.com>
2023-10-20 13:56:46 +0530

    Add files via upload

---
+++ 04-Multiplication/multiplication.c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+int a = 10;
+ int b = 20;
+ int mul = 0;
+ 
+ mul = a * b;
+ 
+ printf("a value =%d\n",a);
+ printf("b value =%d\n",b);
+ printf("mul value =%d\n",mul);
+ 
+ printf("a address =%d\n",&a);
+ printf("b address =%d\n",&b);
+ printf("mul address =%d\n",&mul);
+ 
+ return (0); 
+}
